6de2431fdbaa1210f93a0fff34c701c4c598e391
Kamil Michalak <01169209@pw.edu.pl>
2023-12-09 22:47:05 +0100

    wstepne przygotowanie

---
+++ Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import TextData
+
+
+-- print strings from list in separate lines
+printLines :: [String] -> IO ()
+printLines xs = putStr (unlines xs)
+
+printInstructions = printLines instructionsText
+
+readCommand :: IO String
+readCommand = do
+    putStr "> "
+    xs <- getLine
+    return xs
+
+-- note that the game loop may take the game state as
+-- an argument, eg. gameLoop :: State -> IO ()
+gameLoop :: IO ()
+gameLoop = do
+    cmd <- readCommand
+    case cmd of
+        "instructions" -> do printInstructions
+                             gameLoop
+        "quit" -> return ()
+        _ -> do printLines ["Unknown command.", ""]
+                gameLoop
+
+main = do
+    printInstructions
+    gameLoop
+
---
+++ TextData.hs
@@ -0,0 +1,21 @@
+module TextData where
+
+instructionsText =
+    [
+        "Używaj komend zgodnie ze składnią Haskell-a.",
+        "dostępne komendy:",
+        "start              -- aby rozpocząć grę.",
+        "n   s   e   w      -- aby iść w danym kierunku.",
+        "take(Object)       -- aby podnieść obiekt.",
+        "look               -- aby się rozejrzeć.",
+        "search             -- aby przeszukać lokacje.",
+        "instructions       -- aby zobaczyć tą wiadomość.",
+        "halt               -- aby zatrzymać grę i wyjść.",
+        "map                -- aby dowiedzieć się gdzie można iść.",
+        "goToMoon           -- aby spróbować polecieć na księżyc.",
+        "stats              -- wyświetla aktualne statystyki gracza.",
+        "quit               -- aby wyjść z Haskell-a.",
+        ""
+    ]
+
+
